88dd071551af3e7e0fd8afa3511520cbf5a86693
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-01-25 19:29:34 +0530

    Add files via upload

---
+++ ZOHO4.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,temp,temp1;
+	scanf("%d",&n);
+	int a[n],count[n],max = 0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		if(max < a[i])
+		{
+			max = a[i];
+		}
+	}
+	int *factor = (int*)calloc(max+1,sizeof(int));
+	
+	for(i = 0;i<n;i++)
+	{
+		for(j = 2;j<= a[i]/2;j++)
+		{
+			if(a[i]%j==0)
+				factor[a[i]]++;
+				
+		}
+	
+	}
+ 	for(i=0;i<n;i++)
+	{
+		for(j = i+1 ;j<=n-1;j++)
+		{
+		
+			if(factor[a[i]] <= factor[a[j]])
+			{
+				temp = a[i];
+		
+				a[i] = a[j];
+				
+				a[j] = temp;
+				
+			}
+		}
+		
+	}
+	
+	for(i=0;i<n;i++)
+	{
+	
+			printf("%d ",a[i]);
+	}
+	
+	
+}
