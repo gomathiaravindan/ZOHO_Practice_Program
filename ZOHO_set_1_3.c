e79af63d28564c5c13214af21e6d395db5cf0623
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-03-23 13:58:24 +0530

    Update ZOHO_set_1_3.c

--- ZOHO_set_1_3.c
+++ ZOHO_set_1_3.c
@@ -1,6 +1,6 @@
 ## Save the string “WELCOMETOZOHOCORPORATION” in a two dimensional array and search 
 for substring like “too” in the two dimensional string both from left to right and from top to bottom.
-w e L C O
+W E L C O
 M E T O Z
 O H O C O
 R P O R A
