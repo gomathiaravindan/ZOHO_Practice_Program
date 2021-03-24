7cb7d609bf523bd8b6292ef27959fe11d165e32e
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-03-24 11:43:47 +0530

    Add files via upload

---
+++ ZOHO_set_1_5.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<string.h>
+struct relation
+{
+	char child[50][50];
+	char father[50][50];
+};
+
+int main()
+{
+	int n,i,j,ind[30],count = 0,count1=0;
+	struct relation rel[100];
+	char str[30],chd[30];
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%s",rel[i].child);
+		scanf("%s",rel[i].father);
+	}
+	printf("Enter the parent to find the grandchildren: ");
+	scanf("%s",str);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("<%s, %s>\n",rel[i].child,rel[i].father);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		if(!(strcmp(rel[i].father,str)))
+		{
+			
+			ind[count1] = i;
+			count1++;
+		
+		}
+		
+	}
+	
+	for(i=0;i<count1;i++)
+	{
+		
+			for(j=0;j<n;j++)
+			{
+				if(!(strcmp(rel[ind[i]].child,rel[j].father)))
+				{
+					count++;
+				}
+	    	}
+	}
+	
+	printf("%d\n",count);
+	
+	
+}
