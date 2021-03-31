19c0b99cd4a022eaf3a803e44b32d2c9e56c35da
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-03-31 23:21:03 +0530

    Add files via upload

---
+++ ZOHO_set_18_2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,target,sum=0,max = 0,diff = 0,flag = 0;
+	
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+		if(max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	int search[max+1];
+	
+	for(i=0;i<n;i++)
+	{
+		search[arr[i]] = 1;
+	}
+	printf("Enter the target number:");
+	scanf("%d",&target);
+	
+	for(i=0;i<n;)
+	{
+		 if(arr[i] < target)
+		 {
+		 	diff = target - arr[i];
+		 	if(search[diff] == 1 && search[arr[i]] == 1)
+		 	{
+		 		flag = 1;
+		 		break;
+			 }
+			 else
+			 	i++;
+		 }
+	}
+	if(flag == 1)
+		printf("true");
+	else
+		printf("false");
+}
