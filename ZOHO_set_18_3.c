adf76ae4fc1b312733b4d246fbfb8ba74e39ccb0
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-03-31 20:30:41 +0530

    Add files via upload

---
+++ ZOHO_set_18_3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int x1,temp;
+	float val,x,sinval,sinx,deno,acc = 0.0001;
+	
+	scanf("%f",&x);
+	int i = 1;
+	
+	x = x * (3.142 / 180.0);
+	x1 = x;
+	sinx = x;
+	sinval = (float)sin(x);
+	do{
+		deno = 2 * i * (2 * i + 1);
+		
+		x1 = -x1 * x * x/deno;
+		sinx = sinx + x1;
+		i++;
+	}while(acc<= sinval - sinx);
+	
+	printf("%.1f",sinx);
+}
