4a1caea3673b333ec7755fd631f71cafad6b1151
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-03-31 19:50:03 +0530

    Update ZOHO_set_18_4.c

--- ZOHO_set_18_4.c
+++ ZOHO_set_18_4.c
@@ -1,10 +1,29 @@
+## Write function to find multipication of 2 numbers using +
+operator You must use minimu possible iterations. 
+Input: 3 , 4
+Output 12 
+
+
+
+
+
 #include<stdio.h>
+int prod(int,int);
 int main()
 {
-	int a,b,prod = 0,max, i =0;
+	int a,b,product = 0,max, i =0;
 	
 	scanf("%d, %d",&a,&b);
 	
+	product = prod(a,b);
+	
+	printf("%d",product);
+	return 0;
+}
+
+int prod(int a,int b)
+{
+	int prod = 0;
 	if(a <= b)
 	{
 		while(i < a)
@@ -21,7 +40,6 @@ int main()
 			i++;
 		}
 	}
+	return prod;
 	
-	printf("%d",prod);
-	return 0;
 }
