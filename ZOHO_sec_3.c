e3bf8b1414d80c7d0bdb7b1a2810f6065672c71a
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-04-05 22:27:28 +0530

    Add files via upload

---
+++ ZOHO_sec_3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char number[50];
+	int to_add_number;
+	
+	printf("Enter a string of number:");
+	scanf("%s",number);
+	
+	printf("Enter a number to add:");
+	
+	scanf("%d",&to_add_number);
+	
+	add_to_single_digit(number,to_add_number);
+	
+	return 0;
+}
+
+
+int add_to_single_digit(char *number,int to_add_number)
+{
+	int ch;
+	
+	for(ch =0;ch<strlen(number);ch++)
+	{
+		printf("%d",(number[ch] - '0') + to_add_number);
+	}
+}
