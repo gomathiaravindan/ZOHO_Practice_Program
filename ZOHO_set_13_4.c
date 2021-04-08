c4603b44d782b15aa3fe916292e5311bb880fac7
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-04-08 10:51:56 +0530

    Update ZOHO_set_13_4.c

--- ZOHO_set_13_4.c
+++ ZOHO_set_13_4.c
@@ -1,3 +1,13 @@
+## .A number is called as binary-decimal if all the digits in the number should be either ‘1’ or ‘0’. Any
+number can be written as a sum of binary-decimals. Our task is to find the minimum number of 
+binary-decimals to represent a number.Input : 32Output : 11 11 10 
+Input : 120
+Output : 110 10
+
+
+
+
+
 #include<stdio.h>
 int main()
 {
