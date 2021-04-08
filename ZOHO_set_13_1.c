fb40f94ab015d48bec626f20294bdfdc021ee0be
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-04-08 10:58:58 +0530

    Add files via upload

---
+++ ZOHO_set_13_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	for(i = 0;i<=n-1;i++)
+	{
+		if(arr[i] > arr[i-1])
+		{
+			printf("%d ",arr[i]);
+		}
+	}
+	return 0;
+}
