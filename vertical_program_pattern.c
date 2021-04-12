581ccb304f29bf5b0f7bdc2a597957044fc829d7
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-04-12 20:53:47 +0530

    Add files via upload

---
+++ vertical_program_pattern.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[30];
+	gets(str);
+	
+	int i,j,mid,count=0,k;
+	
+	mid = strlen(str)/2;
+	
+	for(i=1;i<=strlen(str);i++)
+	{
+		for(j = 1;j<=strlen(str);j++)
+		{
+			if(j <= strlen(str) - i)
+			{
+				printf(" ");
+			}
+			
+		}	
+		for(k=0;k<=count;k++)
+		{
+			printf("%c",str[(mid+k)%strlen(str)]);
+			
+		}
+	    count++;
+		printf("\n");
+			
+		}
+		
+		
+	}
+	
+
