9b313e8e82162575a3665372ee12ccfabb1d135f
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-04-20 11:28:00 +0530

    Add files via upload

---
+++ ZOHO_sec_7.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i,j,n,n1,*arr,*arr1,*occurance,max = 0,max1 = 0;
+	
+	printf("Enter the size of the first array:");
+	scanf("%d",&n);
+	
+	arr = (int*)malloc(n*sizeof(int));
+	
+	for(i = 0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+		if(max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	
+	printf("Enter the size of the second array:");
+	scanf("%d",&n1);
+	
+	arr1 = (int*)malloc(n1*sizeof(int));
+	
+	for(i=0;i<n1;i++)
+	{
+		scanf("%d",&arr1[i]);
+		if(max1 < arr1[i])
+		{
+			max1  = arr[i];
+		}
+	}
+	
+	if(max <= max1)
+	{
+		occurance = (int*)calloc(max1+1,sizeof(int));
+		for(i = 0;i<n;i++)
+		{
+			occurance[arr[i]]++;
+		}
+		
+		for(i=0;i<n1;i++)
+		{
+			occurance[arr1[i]]--;
+		}
+		
+		for(i = 0;i<max1+1;i++)
+		{
+			if(occurance[i] != 0)
+			{
+				break;
+			}
+		}
+		
+		for(j = 0;j<n;j++)
+		{
+			if(arr[j] == i)
+			{
+				printf("%d %d",arr[j],j);
+				break;
+			}
+		}
+			for(j = 0;j<n1;j++)
+		{
+			if(arr1[j] == i)
+			{
+				printf("%d %d",arr1[j],j+1);
+				break;
+			}
+		}
+	}
+	else
+	{
+		occurance = (int*)calloc(max+1,sizeof(int));
+		for(i = 0;i<n;i++)
+		{
+			occurance[arr[i]]++;
+
+		}
+		
+		for(i=0;i<n1;i++)
+		{
+			occurance[arr1[i]]--;
+			
+		}
+		
+		for(i = 0;i<max+1;i++)
+		{
+			if(occurance[i] != 0)
+			{
+				break;
+			}
+		}
+		
+		for(j = 0;j<n;j++)
+		{
+			if(arr[j] == i)
+			{
+				printf("%d %d",arr[j],j+1);
+				break;
+			}
+		}
+		for(j = 0;j<n1;j++)
+		{
+			if(arr1[j] == i)
+			{
+				printf("%d %d",arr1[j],j+1);
+				break;
+			}
+		}
+	}
+	free(arr);
+	free(arr1);
+	free(occurance);
+}
