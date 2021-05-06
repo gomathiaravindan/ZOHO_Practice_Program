d42053c13937a5320cccf3958632649e32208ed2
gomathiaravindan <54173246+gomathiaravindan@users.noreply.github.com>
2021-05-06 16:25:57 +0530

    Update ZOHO_set_8_1.c

--- ZOHO_set_8_1.c
+++ ZOHO_set_8_1.c
@@ -7,7 +7,7 @@ Output
 	4322234
 	4321234
 	4333334
-	4222234
+	4322234
 	4444444
 
 
@@ -27,7 +27,7 @@ int main()
 	{
 		for(j=0;j<(2 * n)-1;j++)
 		{
-			printf("%d",abs(n - i - 1)> abs(n - j -1) ? abs(n - i - 1) :abs(n - j -1) + 1);
+			printf("%d",(abs(n - i - 1)> abs(n - j -1) ? abs(n - i - 1) :abs(n - j -1)) + 1);
 		}
 		printf("\n");
 	}
